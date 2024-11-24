e28e094c0db941fd8fd892ce30613629bc005a23
rachmethe <ugandasabo@gmail.com>
2024-11-24 06:09:17 +0300

    Switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 
 function App() {
   return (
-    <Router basename='goryaninsky-react-portfolio'>
-      <div className='bg-transparent relative min-h-screen w-screen overflow-hidden'>
+    <Router>
+      <div className="bg-transparent relative min-h-screen w-screen overflow-hidden">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
